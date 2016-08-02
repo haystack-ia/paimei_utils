13cc2593bbf6f0956d1c7e0092976cb02a3e429f
Haystack <haystackinfosec@gmail.com>
2016-08-02 14:50:53 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "paimei_utils",
+    version = "0.1",
+    author = "haystack",
+    author_email = "haystackinfosec@gmail.com",
+    description = "a subset of PaiMei utils that are useful outside of PaiMei",
+    license="GPL",
+    packages=["paimei_utils"],
+)
